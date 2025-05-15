356b4122fa62a22c22412ba55ac12b4f2f8948cd
Yuta Tezuka <yuta.tzk@gmail.com>
2025-05-15 20:05:02 +0900

    Mult.asmを作成

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,43 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+    // sum = 0
+    @sum
+    M=0
+
+    // i = 0
+    @i
+    M=0
+
+(LOOP)
+    // if (i >= R1) goto STOP
+    @i
+    D=M
+    @R1
+    D=D-M
+    @STOP
+    D;JGE
+
+    // sum = sum + R0
+    @R0
+    D=M
+    @sum
+    M=D+M
+
+    // i = i + 1
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(STOP)
+    // R2 = sum
+    @sum
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0;JMP
