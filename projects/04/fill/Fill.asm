3d99e1fab7146e3ed3f4d224b876b3fc1391a471
Yuta Tezuka <yuta.tzk@gmail.com>
2025-05-15 22:04:37 +0900

    Fill.asmを作成

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,3 +8,74 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+    // num_rams = 32 // 変更対象のRAMの総数
+    @32 // 本当はディスプレイの全ての色を変更しなければならないが、時間がかかるため1行分のみ変更
+    D=A
+    @num_rams
+    M=D
+
+(LOOP)
+    // if (KBD == 0) goto WHITHEN, else goto BLAKEN
+    @KBD
+    D=M
+    @WHITEN
+    D;JEQ
+    @BLAKEN
+    0;JMP
+
+(WHITEN)
+    // i = 0
+    @i
+    M=0
+
+(WHITEN_LOOP)
+    // if (num_rams == i) goto LOOP
+    @num_rams
+    D=M
+    @i
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // *(SCREEN + i) = 0
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=0
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    @WHITEN_LOOP
+    0;JMP
+
+(BLAKEN)
+    // i = 0
+    @i
+    M=0
+
+(BLAKEN_LOOP)
+    // if (num_rams == i) goto LOOP
+    @num_rams
+    D=M
+    @i
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // *(SCREEN + i) = 0
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=-1
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    @BLAKEN_LOOP
+    0;JMP
